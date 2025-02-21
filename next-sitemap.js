54d88d59ef454e5298d1fa155bfd26bc70e4938c
gaurav551 <gaurav.ga6@gmail.com>
2025-02-21 13:48:27 +0545

    removed next-sitemap

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://flickday.to', // Change this to your actual domain
-    generateRobotsTxt: true, // Automatically creates robots.txt
-    sitemapSize: 5000, // Maximum URLs per sitemap file
-  };
-  
