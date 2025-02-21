03db3ece180b008d837dac8fae7a3993ce1cb522
gaurav551 <gaurav.ga6@gmail.com>
2025-02-21 13:28:11 +0545

    sitemap and google console code

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://flickday.to', // Change this to your actual domain
+    generateRobotsTxt: true, // Automatically creates robots.txt
+    sitemapSize: 5000, // Maximum URLs per sitemap file
+  };
+  
