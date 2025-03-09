7250d61c7db47ccf03cad3482c1ed599a8bd41da
gaurav551 <gaurav.ga6@gmail.com>
2025-03-09 19:12:52 +0545

    suspense

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -6,24 +6,29 @@ import SiteProviders from "./SiteProvider";
 import Sidebar from "./Sidebar";
 import Header from "@/components/header";
 
-export default function SiteLayout({ children }: { children: React.ReactNode }) {
+export default function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   let googleAnalyticsCode = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
   return (
     <SiteProviders>
       <div className="flex">
         {/* Sidebar with fixed position */}
-        <Sidebar />
-        
+
+        <Suspense fallback={<></>}>
+          {/* The component that might need time to load */}
+          <Sidebar />
+        </Suspense>
         {/* Main content with margin to account for sidebar */}
-        <div className="flex flex-col min-h-screen w-full md:ml-14 overflow-x-hidden"> 
-          <Header />
-          
-          <Suspense fallback={<></>} />
-          
-          <main className="flex-1">
-            {children}
-          </main>
+        <div className="flex flex-col min-h-screen w-full md:ml-14 overflow-x-hidden">
+          <Suspense fallback={<></>}>
+            {/* The component that might need time to load */}
+            <Header />
+          </Suspense>
+          <main className="flex-1">{children}</main>
 
           <Footer />
         </div>
