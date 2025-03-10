cec9401c1383f0afc44719c8e03b5cef3c5588e3
gaurav551 <gaurav.ga6@gmail.com>
2025-03-10 13:00:25 +0545

    login/signup issue fix with svg to icon react-query

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,81 +1,85 @@
 "use client";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { toast } from "react-hot-toast";
 import { googleHandler } from "@/utils/authUtils";
 import { useForm } from "react-hook-form";
-import { GetUser, UserLogin} from "@/services/AccountService";
+import { GetUser, UserLogin } from "@/services/AccountService";
 import { useAuth } from "@/app/context/AuthContext";
 import Spin from "../common/Spin";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { FaGoogle, FaArrowRight, FaCheck } from "react-icons/fa";
 
 const Login = ({ code }: { code?: string }) => {
-  //for login
   const baseUrl = process.env.NEXT_PUBLIC_APP_BASE_URL;
-  const [loading, setLoading] = useState(false);
-
   const { login } = useAuth();
-
   const router = useRouter();
+  
   const {
     register,
     handleSubmit,
-  
     formState: { errors },
   } = useForm();
   
-  useEffect(() => {
-   
-      if (code) {
-        console.log("Sending request with code:", code);
-        setLoading(true);
-        GetUser(code as string)
-          .then((res:any) => {
-            if (res) {
-              login(res.data); // Using the auth context login
-              router.push("/");
-              toast.success("Login successful",
-                { position: "bottom-center" },
-              );
-            }
-          })
-          .catch((err) => {
-            toast.error(err?.response?.data?.message || "An error occurred. Please try again.", { position: 'bottom-center' });
-            console.log(err);
-          }).finally(()=> setLoading(false));
-      
+  // Login mutation using React Query
+  const loginMutation = useMutation({
+    mutationFn: (data) => UserLogin(data),
+    onSuccess: (res) => {
+      login(res.data);
+      router.push("/");
+      toast.success("Login successful", {
+        position: "bottom-center"
+      });
+    },
+    onError: (err: any) => {
+      toast.error(
+        err?.response?.data?.message ||
+          "An error occurred. Please try again.",
+        { position: "bottom-center" }
+      );
     }
-    
-  }, [code]);
-
-  const onSubmit = (data) => {
-    UserLogin(data)
-      .then((res) => {
-        login(res.data); // Using the auth context login
+  });
+  
+  // Google Auth mutation using React Query
+  const googleAuthMutation = useMutation({
+    mutationFn: (authCode: string) => GetUser(authCode),
+    onSuccess: (res: any) => {
+      if (res) {
+        login(res.data);
         router.push("/");
         toast.success("Login successful", {
           position: "bottom-center"
         });
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error(
-          err?.response?.data?.message ||
-            "An error occurred. Please try again.",
-          { position: "bottom-center" }
-        );
-      });
+      }
+    },
+    onError: (err: any) => {
+      toast.error(
+        err?.response?.data?.message ||
+          "An error occurred. Please try again.",
+        { position: "bottom-center" }
+      );
+    }
+  });
+
+  useEffect(() => {
+    if (code) {
+      console.log("Sending request with code:", code);
+      googleAuthMutation.mutate(code);
+    }
+  }, [code]);
+
+  const onSubmit = (data) => {
+    loginMutation.mutate(data);
   };
 
   return (
     <>
       {/* <!-- ===== Login Form Start ===== --> */}
-      <section className="pb-10  md:pt-2 xl:pt-1">
-      <div className="relative z-1 mx-auto max-w-c-1016 px-4 pb-7.5 pt-10 sm:px-7.5 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
-          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg "></div>
-         
+      <section className="pb-10 md:pt-2 xl:pt-1">
+        <div className="relative z-1 mx-auto max-w-c-1016 px-4 pb-7.5 pt-10 sm:px-7.5 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg"></div>
 
           <motion.div
             variants={{
@@ -83,7 +87,6 @@ const Login = ({ code }: { code?: string }) => {
                 opacity: 0,
                 y: -20,
               },
-
               visible: {
                 opacity: 1,
                 y: 0,
@@ -96,7 +99,7 @@ const Login = ({ code }: { code?: string }) => {
             className="animate_top rounded-lg bg-white px-4 pt-7.5 shadow-solid-8 dark:border dark:border-strokedark dark:bg-gray-900 sm:px-7.5 xl:px-15 xl:pt-15"
           >
             <h2 className="mb-15 text-center text-2xl font-semibold text-black dark:text-white sm:text-3xl xl:text-sectiontitle2">
-              Login to Your Account 
+              Login to Your Account
             </h2>
             <div className="flex flex-col">
               <div className="flex items-center gap-4 sm:gap-8">
@@ -104,42 +107,18 @@ const Login = ({ code }: { code?: string }) => {
                   onClick={() => googleHandler(`${baseUrl}auth/login`)}
                   aria-label="sign with google"
                   className="text-body-color dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border border-stroke bg-[#f8f8f8] px-4 py-3 text-base outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none sm:px-6"
+                  disabled={googleAuthMutation.isPending}
                 >
-                   {loading? <Spin/> : <> <span className="mr-3">
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g clipPath="url(#clip0_95:967)">
-                        <path
-                          d="M20.0001 10.2216C20.0122 9.53416 19.9397 8.84776 19.7844 8.17725H10.2042V11.8883H15.8277C15.7211 12.539 15.4814 13.1618 15.1229 13.7194C14.7644 14.2769 14.2946 14.7577 13.7416 15.1327L13.722 15.257L16.7512 17.5567L16.961 17.5772C18.8883 15.8328 19.9997 13.266 19.9997 10.2216"
-                          fill="#4285F4"
-                        />
-                        <path
-                          d="M10.2042 20.0001C12.9592 20.0001 15.2721 19.1111 16.9616 17.5778L13.7416 15.1332C12.88 15.7223 11.7235 16.1334 10.2042 16.1334C8.91385 16.126 7.65863 15.7206 6.61663 14.9747C5.57464 14.2287 4.79879 13.1802 4.39915 11.9778L4.27957 11.9878L1.12973 14.3766L1.08856 14.4888C1.93689 16.1457 3.23879 17.5387 4.84869 18.512C6.45859 19.4852 8.31301 20.0005 10.2046 20.0001"
-                          fill="#34A853"
-                        />
-                        <path
-                          d="M4.39911 11.9777C4.17592 11.3411 4.06075 10.673 4.05819 9.99996C4.0623 9.32799 4.17322 8.66075 4.38696 8.02225L4.38127 7.88968L1.19282 5.4624L1.08852 5.51101C0.372885 6.90343 0.00012207 8.4408 0.00012207 9.99987C0.00012207 11.5589 0.372885 13.0963 1.08852 14.4887L4.39911 11.9777Z"
-                          fill="#FBBC05"
-                        />
-                        <path
-                          d="M10.2042 3.86663C11.6663 3.84438 13.0804 4.37803 14.1498 5.35558L17.0296 2.59996C15.1826 0.901848 12.7366 -0.0298855 10.2042 -3.6784e-05C8.3126 -0.000477834 6.45819 0.514732 4.8483 1.48798C3.2384 2.46124 1.93649 3.85416 1.08813 5.51101L4.38775 8.02225C4.79132 6.82005 5.56974 5.77231 6.61327 5.02675C7.6568 4.28118 8.91279 3.87541 10.2042 3.86663Z"
-                          fill="#EB4335"
-                        />
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_95:967">
-                          <rect width="20" height="20" fill="white" />
-                        </clipPath>
-                      </defs>
-                    </svg>
-                  </span>
-                  Continue with Google</>}
-
+                  {googleAuthMutation.isPending ? (
+                    <Spin />
+                  ) : (
+                    <>
+                      <span className="mr-3">
+                        <FaGoogle className="text-primary" size={20} />
+                      </span>
+                      Continue with Google
+                    </>
+                  )}
                 </button>
               </div>
             </div>
@@ -154,36 +133,39 @@ const Login = ({ code }: { code?: string }) => {
 
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-7.5 flex flex-col gap-7.5 lg:mb-12.5 lg:flex-row lg:justify-between lg:gap-14">
-                <input
-                  placeholder="Email"
-                  {...register("email", { required: "Email is required" })}
-                  type="email"
-                  className="w-full border-b border-stroke bg-transparent pb-2.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white"
-                />
-                {errors.email && (
-                  <p className="text-red-500">
-                    {errors.email.message?.toString()}
-                  </p>
-                )}
+                <div className="w-full">
+                  <input
+                    placeholder="Email"
+                    {...register("email", { required: "Email is required" })}
+                    type="email"
+                    className="w-full border-b border-stroke bg-transparent pb-2.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white"
+                  />
+                  {errors.email && (
+                    <p className="mt-1 text-red-500 text-sm">
+                      {errors.email.message?.toString()}
+                    </p>
+                  )}
+                </div>
 
-                <input
-                  {...register("password", {
-                    required: "Password is required",
-                    minLength: {
-                      value: 6,
-                      message: "Password must be at least 6 characters long",
-                    },
-                  })}
-                  type="password"
-                  placeholder="Password"
-                  
-                  className="w-full border-b border-stroke bg-transparent pb-2.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white"
-                />
-                 {errors.password && (
-                  <p className="text-red-500">
-                    {errors.password.message?.toString()}
-                  </p>
-                )}
+                <div className="w-full">
+                  <input
+                    {...register("password", {
+                      required: "Password is required",
+                      minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters long",
+                      },
+                    })}
+                    type="password"
+                    placeholder="Password"
+                    className="w-full border-b border-stroke bg-transparent pb-2.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white"
+                  />
+                  {errors.password && (
+                    <p className="mt-1 text-red-500 text-sm">
+                      {errors.password.message?.toString()}
+                    </p>
+                  )}
+                </div>
               </div>
 
               <div className="flex flex-wrap items-center gap-4 sm:gap-10 md:justify-between xl:gap-15">
@@ -195,21 +177,10 @@ const Login = ({ code }: { code?: string }) => {
                       className="peer sr-only"
                     />
                     <span className="group mt-1 flex h-5 min-w-[20px] items-center justify-center rounded border-gray-300 bg-gray-100 text-blue-600 peer-checked:bg-primary dark:border-gray-600 dark:bg-gray-700">
-                      <svg
+                      <FaCheck
                         className="opacity-0 peer-checked:group-[]:opacity-100"
-                        width="10"
-                        height="8"
-                        viewBox="0 0 10 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M9.70704 0.792787C9.89451 0.980314 9.99983 1.23462 9.99983 1.49979C9.99983 1.76495 9.89451 2.01926 9.70704 2.20679L4.70704 7.20679C4.51951 7.39426 4.26521 7.49957 4.00004 7.49957C3.73488 7.49957 3.48057 7.39426 3.29304 7.20679L0.293041 4.20679C0.110883 4.01818 0.0100885 3.76558 0.0123669 3.50339C0.0146453 3.24119 0.119814 2.99038 0.305222 2.80497C0.490631 2.61956 0.741443 2.51439 1.00364 2.51211C1.26584 2.50983 1.51844 2.61063 1.70704 2.79279L4.00004 5.08579L8.29304 0.792787C8.48057 0.605316 8.73488 0.5 9.00004 0.5C9.26521 0.5 9.51951 0.605316 9.70704 0.792787Z"
-                          fill="white"
-                        />
-                      </svg>
+                        size={10}
+                      />
                     </span>
                     <label
                       htmlFor="default-checkbox"
@@ -225,23 +196,19 @@ const Login = ({ code }: { code?: string }) => {
                 </div>
 
                 <button
+                  type="submit"
                   aria-label="login with email and password"
                   className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
+                  disabled={loginMutation.isPending}
                 >
-                  Log in
-                  <svg
-                    className="fill-white"
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10.4767 6.16664L6.00668 1.69664L7.18501 0.518311L13.6667 6.99998L7.18501 13.4816L6.00668 12.3033L10.4767 7.83331H0.333344V6.16664H10.4767Z"
-                      fill=""
-                    />
-                  </svg>
+                  {loginMutation.isPending ? (
+                    <Spin />
+                  ) : (
+                    <>
+                      Log in
+                      <FaArrowRight size={14} />
+                    </>
+                  )}
                 </button>
               </div>
 
