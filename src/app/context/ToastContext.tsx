16d2b4252345d8dae8f74553fcbf9d79b76a5451
gaurav551 <gaurav.ga6@gmail.com>
2025-03-12 19:26:32 +0545

    toaster background changed

--- src/app/context/ToastContext.tsx
+++ src/app/context/ToastContext.tsx
@@ -2,10 +2,20 @@
 import { Toaster } from "react-hot-toast";
 
 const ToasterContext = () => {
-  
   return (
     <div>
-      <Toaster  position="bottom-center" reverseOrder={false} />
+      <Toaster 
+        position="bottom-center" 
+        reverseOrder={false}
+        
+        toastOptions={{
+          duration: 2000, // Duration in milliseconds (5 seconds)
+          style: {
+            background: "black",
+            color: "white",
+          },
+        }}
+      />
     </div>
   );
 };
