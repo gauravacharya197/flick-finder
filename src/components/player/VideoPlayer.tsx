0926970fd937ce1b953305a24e411ba4a0c24e00
gaurav551 <gaurav.ga6@gmail.com>
2025-03-21 17:31:59 +0545

    changed vidplayer loading

--- src/components/player/VideoPlayer.tsx
+++ src/components/player/VideoPlayer.tsx
@@ -1,34 +1,27 @@
 import { useState, useEffect } from 'react';
 
 const VidPlayer = ({ sourceUrl, sandbox = '' }) => {
-  const [showLoader, setShowLoader] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   
   // Function to handle iframe load event
   const handleIframeLoad = () => {
+    setIsLoading(false);
   };
   
   // Reset loading state when source URL changes
-  useEffect(() => {
-    setShowLoader(true);
-    
-    // Only show loading for 500ms
-    const timer = setTimeout(() => {
-      setShowLoader(false);
-    }, 500);
-    
-    return () => clearTimeout(timer);
-  }, [sourceUrl]);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  // }, [sourceUrl]);
   
   return (
     <div className="relative w-full h-[40vh] sm:h-[70vh] xl:h-[72vh] 2xl:h-[65vh] bg-black">
-      {/* Blurred background with centered loading indicator */}
-      {showLoader && (
+       {/* {isLoading && (
          <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur">
           <div className="flex items-center space-x-2">
             <div className="h-6 w-6 animate-spin rounded-full border-b-2 border-primary"></div>
           </div>
         </div>
-      )}
+      )} */}
       
       <iframe
         src={sourceUrl}
