7d23c6d4a3147e65587dc30fb1b1c2c25af01249
Gaurav A. <33540578+gaurav551@users.noreply.github.com>
2025-03-21 18:24:23 +0545

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
+// this function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // Get the pathname
   const path = request.nextUrl.pathname;
