0f45266bc77bc4686501c121f281828e8bbdffa9
gaurav551 <gaurav.ga6@gmail.com>
2025-03-22 20:40:30 +0545

    site description and videoserver

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,17 +4,28 @@ import MovieHomepage from "./home/page";
 import Container from "@/components/common/Container";
 
 export const metadata: Metadata = {
-  title: siteConfig.title,
-  description: siteConfig.description,
-  // other metadata
+  title: `${siteConfig.title}`,
+  description: `${siteConfig.description}`,
+  keywords: ['free movies', 'TV shows online', 'streaming', 'watch online', 'HD movies', siteConfig.siteName],
+  alternates: {
+    canonical: siteConfig.url,
+  },
+ authors: [
+    { name: 'Flickday', url: siteConfig.url },
+  ],
+  openGraph: {
+    title: siteConfig.title,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: siteConfig.siteName,
+    type: 'website',
+  },
 };
 
 export default function HomePage() {
- 
   return (
-    <>
-      <><MovieHomepage/></>
-
-    </>
+    <Container>
+      <MovieHomepage />
+    </Container>
   );
 }
--- src/app/(site)/watch/[mediaType]/[id]/[[...title]]/page.tsx
+++ src/app/(site)/watch/[mediaType]/[id]/[[...title]]/page.tsx
@@ -1,10 +1,12 @@
+
+
 import Container from '@/components/common/Container';
-import { Genre } from '@/components/genre/Genre';
 import Watch from '@/components/watch';
 import { siteConfig } from '@/config/siteConfig';
 import { capitalizeFirstLetter } from '@/utils/capitalizeFirstLetter';
 import { Metadata } from 'next';
 import React from 'react';
+
 export type WatchPageProps = {
   params: Promise<{
     mediaType: string,
@@ -12,30 +14,37 @@ export type WatchPageProps = {
     title?: string
   }>
 }
-export  async function generateMetadata({params}:   WatchPageProps) {
+
+export async function generateMetadata({ params }: WatchPageProps): Promise<Metadata> {
   const { mediaType, id, title } = await params;
+  
   // Decode and format the title if available, otherwise use a default
   const formattedTitle = title 
     ? decodeURIComponent(title).replace(/-/g, ' ') 
     : `${capitalizeFirstLetter(mediaType)}`;
-  const capitalizedTitle = capitalizeFirstLetter(formattedTitle)
-
+  const capitalizedTitle = capitalizeFirstLetter(formattedTitle);
+  
+  
   const description = title 
-    ? `Watch ${capitalizedTitle} online. Explore a wide selection of free movies and TV shows on ${siteConfig.siteName}. ${siteConfig.description}`
-    : `Find and watch movies and TV shows on ${siteConfig.siteName}. ${siteConfig.description}`;
+  ? `Watch ${capitalizedTitle} online. Explore a wide selection of free movies and TV shows on ${siteConfig.siteName}. ${siteConfig.description}`
+  : `Find and watch movies and TV shows on ${siteConfig.siteName}. ${siteConfig.description}`;
 
   return {
     title: `${capitalizedTitle} - Watch ${capitalizedTitle} Online | ${siteConfig.siteName}`,
     description,
+    keywords: [capitalizedTitle, `watch ${formattedTitle}`, mediaType, siteConfig.siteName],
+    alternates: {
+      canonical: `${siteConfig.url}/watch/${mediaType}/${id}/${title || ''}`,
+    },
   };
 }
 
-export default async function WatchPage({params}:   WatchPageProps) {
-  const genre = (await params);
+export default async function WatchPage({ params }: WatchPageProps) {
+  const mediaData = await params;
 
   return (
-    <Container >
-      <Watch params={genre}/>
+    <Container>
+      <Watch params={mediaData} />
     </Container>
   );
 }
